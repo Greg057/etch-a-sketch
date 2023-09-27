d4aca9a2debd653b629eb8b68fb19453b6eb4300
Greg057 <88341092+Greg057@users.noreply.github.com>
2023-09-27 12:12:51 +0200

    Finish grid sizing

--- logic.js
+++ logic.js
@@ -1,15 +1,29 @@
 const grid = document.querySelector("#grid");
+const sizeBtn = document.querySelector("#size-button")
+let size = 16;
+changeGrid (size);
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    grid.appendChild(row);
-    for (let j = 0; j < 16; j++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        square.addEventListener("mouseover", function (square) {
-            square.target.style.background = "black";
-        })
-        row.appendChild(square);
+sizeBtn.addEventListener("click", () => {
+    do {
+        size = +prompt("Size of grid: "); 
+    } while (size > 100);
+    changeGrid (size);
+})
+
+function changeGrid (n) {
+    grid.replaceChildren();
+    for (let i = 0; i < n; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        grid.appendChild(row);
+        for (let j = 0; j < n; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.addEventListener("mouseover", function (square) {
+                square.target.style.background = "black";
+            })
+            row.appendChild(square);
+        }
     }
 }
+
