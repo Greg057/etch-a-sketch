179308dd4f3ab7421deb905091fec6996d75025f
Greg057 <88341092+Greg057@users.noreply.github.com>
2023-09-27 11:39:51 +0200

    Change background color div to black when mouse over

--- logic.js
+++ logic.js
@@ -7,8 +7,9 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const square = document.createElement("div");
         square.classList.add("square");
+        square.addEventListener("mouseover", function (square) {
+            square.target.style.background = "black";
+        })
         row.appendChild(square);
     }
 }
-
-
