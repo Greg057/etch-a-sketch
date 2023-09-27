a1c77656e7e790b1b22b49dbe416645acb3a35be
Greg057 <88341092+Greg057@users.noreply.github.com>
2023-09-27 13:13:16 +0200

    Finish extra features

--- logic.js
+++ logic.js
@@ -1,7 +1,25 @@
 const grid = document.querySelector("#grid");
+const colorButtons = document.querySelectorAll(".colorBtn");
+const resetBtn = document.querySelector("#reset");
 const sizeBtn = document.querySelector("#size-button")
+const squares = []
+
 let size = 16;
+let colorBackground = "black";
+
 changeGrid (size);
+
+resetBtn.addEventListener("click", () => {
+    squares.forEach((square) => {
+        square.style.background = "white";
+    })
+})
+
+colorButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        colorBackground = button.getAttribute("id");
+    })
+})
 
 sizeBtn.addEventListener("click", () => {
     do {
@@ -18,12 +36,18 @@ function changeGrid (n) {
         grid.appendChild(row);
         for (let j = 0; j < n; j++) {
             const square = document.createElement("div");
+            squares.push(square);
             square.classList.add("square");
-            square.addEventListener("mouseover", function (square) {
-                square.target.style.background = "black";
+            square.addEventListener("mouseover", () => {
+                if (colorBackground == "black") {
+                    square.style.background = "black";
+                }
+                else {
+                    square.style.background = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;;
+                }
+                
             })
             row.appendChild(square);
         }
     }
 }
-
